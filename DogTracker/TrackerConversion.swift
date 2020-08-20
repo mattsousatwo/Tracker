865376cd8808d9b5bcb1f34ca95915cfc7c32e35
Matt Sousa <mattsousatwo@gmail.com>
2020-08-20 03:57:17 -0400

    Added TimeRow

---
+++ DogTracker/TrackerConversion.swift
@@ -0,0 +1,9 @@
+//
+//  TrackerConversion.swift
+//  DogTracker
+//
+//  Created by Matthew Sousa on 8/16/20.
+//  Copyright © 2020 Matthew Sousa. All rights reserved.
+//
+
+import Foundation
---
+++ DogTracker/View/SwiftUIView.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftUIView.swift
+//  DogTracker
+//
+//  Created by Matthew Sousa on 8/17/20.
+//  Copyright © 2020 Matthew Sousa. All rights reserved.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
