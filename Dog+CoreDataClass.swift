c817591f7dbf06ce5d715758b1b113388d2982f1
Matthew Sousa <mattsousatwo@gmail.com>
2021-03-06 19:44:54 -0500

    Added Dog Class
    - added Dog to coredata

---
+++ Dog+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Dog+CoreDataClass.swift
+//  DogTracker
+//
+//  Created by Matthew Sousa on 3/6/21.
+//  Copyright © 2021 Matthew Sousa. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Dog)
+public class Dog: NSManagedObject {
+
+}
---
+++ Dog+CoreDataProperties.swift
@@ -0,0 +1,30 @@
+//
+//  Dog+CoreDataProperties.swift
+//  DogTracker
+//
+//  Created by Matthew Sousa on 3/6/21.
+//  Copyright © 2021 Matthew Sousa. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Dog {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Dog> {
+        return NSFetchRequest<Dog>(entityName: "Dog")
+    }
+
+    @NSManaged public var uuid: Int16
+    @NSManaged public var name: String?
+    @NSManaged public var weight: Double
+    @NSManaged public var breed: String?
+    @NSManaged public var birthdate: String?
+
+}
+
+extension Dog : Identifiable {
+
+}
